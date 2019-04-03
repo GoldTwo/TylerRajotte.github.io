e731898fb41f8a35b698a397a9c0b6d3e4fd36d9
TylerRajotte <tyler.rajotte087@rdpsd.ab.ca>
2019-04-03 15:19:48 -0600

    Auto Commit Attempts...

--- gittest.py
+++ gittest.py
@@ -1,5 +1,7 @@
 import subprocess
 message = "Auto Commit Attempts..."
+username = input("Username: ")
+password = input("Password: ")
 
 addlist = ["git", "add", "."]
 addcmd = "git add ."
@@ -11,5 +13,18 @@ subprocess.call(commitcmd, shell=True)
 
 pushlist = ["git", "push"]
 pushcmd = "git push"
-push = subprocess.Popen(pushlist, stdout=subprocess.PIPE).communicate()
-print(push)
+push = subprocess.Popen(pushcmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+push.communicate(input=username)
+push.communicate(input=password)
+
+# It might be better to use communicate:
+#
+# from subprocess import Popen, PIPE, STDOUT
+# p = Popen(['myapp'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+# stdout_data = p.communicate(input='data_to_write')[0]
+#
+# "Better", because of this warning:
+#
+#     Use communicate() rather than .stdin.write, .stdout.read or .stderr.read to avoid deadlocks due to any of the other OS pipe buffers filling up and blocking the child process.
+#
+
