262542086ac0211b69164fb723e3dd5e543d9ba8
TylerRajotte <tyler.rajotte087@rdpsd.ab.ca>
2019-06-12 15:34:56 -0600

    Added Best of feature to autobuild

--- sitebuild.py
+++ sitebuild.py
@@ -7,22 +7,101 @@ import git
 
 class SiteBuild(object):
     def __init__(self):
+        self.data = []
+        self.bestofdata = []
+
         self.homebody = []
         self.homebodystring = ""
-        self.data = []
+
+        self.bestofbody = []
+        self.bestofstring = ""
 
         self.__import()
         print("Imported Data")
+        self.__importbestof()
+        print("Imported Bestof Data")
+
         self.__newpost()
         print("Created Posts")
         self.__compileposts()
         print("Compiled Posts")
         self.__export()
         print("Exported Code Block")
+
+        self.__newbestofposts()
+        print("Created Best of Posts")
+        self.__compilebestofposts()
+        print("Compiled best of Posts")
+        self.__exportbestof()
+        print("Exported Best of Code Block")
+
         self.__deploy()
         print("Site Deployed")
 
         print("Site Built!")
+
+    def __importbestof(self):
+        bestoffile = open("bestof/BestOf20182019.txt", "r")
+        formatedbestof = bestoffile.read().split("\n")
+        bestoffile.close()
+        del formatedbestof[0]
+        for i in formatedbestof:
+            if i == "":
+                continue
+            else:
+                self.bestofdata.append(i)
+
+    def __newbestofposts(self):
+        # New post on homepage
+        for entry in self.bestofdata:
+            for potentialpost in self.data:
+                if potentialpost == "":
+                    continue
+                potentialpostformatted = potentialpost.split(",")
+                if potentialpostformatted[4] == entry:
+                    self.__createbestofpost(potentialpostformatted)
+
+    def __createbestofpost(self, entry):
+        # Generates the html and appends it to main body array
+        # Each item in the homebody array is a post containing the html
+        title = entry[0]
+        fontsize = entry[1]
+        date = self.__convertdate(entry[2])
+        titleimage = "./images/icon/" + entry[3]
+        pagename = "./pages/" + entry[4] + ".html"
+
+        template = [
+            "      <div class=\"flexboxchild\" style=\"background-image: url(\'{}\')\">\n".format(titleimage),
+            "        <a class=\"hiddenlink\" href=\"{}\">\n".format(pagename),
+            "          <div class=\"childcontainer\">\n",
+            "            <div class=\"childtitle {}text\">{}</div>\n".format(fontsize, title),
+            "            <div class=\"childdate\">{}</div>\n".format(date),
+            "          </div>\n",
+            "        </a>\n",
+            "      </div>\n"]
+
+        self.bestofbody.append(template)
+
+    def __compilebestofposts(self):
+        # Converts the array to a string
+        compileposts = ""
+        for post in self.bestofbody:
+            for codeline in post:
+                compileposts = compileposts + codeline
+        self.bestofstring = compileposts
+
+    def __exportbestof(self):
+        # Takes compiled string and copy pastes it on to the template
+        indextemplatefile = open("template/bestofTemplate.html", "r")
+        indextemplate = indextemplatefile.read().split("\n")
+        indextemplatefile.close()
+
+        indexoutput = open("bestof20182019.html", "w")
+        for line in indextemplate:
+            if line == "|body|":
+                indexoutput.write(self.bestofstring)
+            else:
+                indexoutput.write(line + "\n")
 
     @staticmethod
     def __deploy():
@@ -34,7 +113,7 @@ class SiteBuild(object):
 
     @staticmethod
     def __convertdate(inputdate):
-        # Converts Dates from numbers to a nicely formated version 
+        # Converts Dates from numbers to a nicely formated version
         monthdata = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
                      "November", "December"]
         splited = inputdate.split("-")
@@ -87,7 +166,7 @@ class SiteBuild(object):
             entry = entry.split(",")
 
             print("Creating: " + str(entry))
-            
+
             self.__createhomepost(entry)
             try:
                 self.__createpage(entry)
